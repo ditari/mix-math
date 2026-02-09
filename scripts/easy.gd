645b4020f5801b3df5a94a0ba2f3182f0ca6051c
ditari <xiratid@gmail.com>
2026-02-09 09:13:33 +0700

    sampai play dan reset button belum timer

---
+++ scripts/easy.gd
@@ -0,0 +1,219 @@
+extends Node2D
+
+var machine_scene: PackedScene = load("res://scenes/machine.tscn")
+var b_empty_scene: PackedScene = load("res://scenes/bottle-empty.tscn")
+var b_choice_scene: PackedScene = load("res://scenes/bottle-choice.tscn")
+
+var b_pour_left_scene: PackedScene = load("res://scenes/bottle-pour-left.tscn")
+var b_pour_right_scene: PackedScene = load("res://scenes/bottle-pour-right.tscn")
+var b_pour_result_scene: PackedScene = load("res://scenes/bottle-pour-result.tscn")
+
+var b_result_scene: PackedScene = load("res://scenes/bottle-result.tscn")
+
+var machine
+var b_empty
+var b_pour_result
+var b_result
+
+var choices = [] # [5,6,7,8]
+var targetnumber #=  11
+
+#index yg dipilih
+var b_index1 = null
+var b_index2 = null
+#result yg dihasilkan
+var result = null
+
+var clicked = 0
+var processed = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#placing the machine
+	machine = machine_scene.instantiate()
+	machine.position = Vector2(360,500) #nanti posisikan dynamic
+	add_child(machine)
+	machine.connect("reset", machine_reset)	
+	machine.connect("go", machine_process)
+	
+	b_empty = b_empty_scene.instantiate()
+	b_empty.position = Vector2(360,775) 
+	add_child(b_empty)
+	
+
+	
+	#generate array choices angka nya dulu 
+	generate_choices_array()
+	#baru generate choice bottle nya
+	generate_choices_bottle()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func generate_choices_array():
+	#randomize number from 0 to 10
+	var numbers = range(11)   # 0–10
+	numbers.shuffle()
+	
+	#get choicesarray
+	for i in range(4):
+		choices.append(numbers[i])
+			
+	#randomize index 0 to 3		
+	var nums = [0, 1, 2, 3]
+	nums.shuffle()
+
+	#get two index
+	var index1 = nums[0]
+	var index2 = nums[1]
+	
+	targetnumber = choices[index1] + choices[index2]
+	$label.text = "Make a " + str(targetnumber) + "!"
+	
+func generate_choices_bottle():
+	for child in $bottle_choice.get_children():
+		child.queue_free()
+	
+	generate_one_bottle(0,choices[0])
+	generate_one_bottle(1,choices[1])
+	generate_one_bottle(2,choices[2])
+	generate_one_bottle(3,choices[3])	
+
+	
+func generate_one_bottle(index, number):
+	var obj = b_choice_scene.instantiate()
+	
+	if index == 0:
+		obj.position = Vector2(200,950) 
+		obj.index = 0 	
+	elif index == 1:
+		obj.position = Vector2(500,950) 
+		obj.index = 1 			
+	elif index == 2:
+		obj.position = Vector2(200,1150) 
+		obj.index = 2 				
+	else :
+		obj.position = Vector2(500,1150) 
+		obj.index = 3 	
+				
+	$bottle_choice.add_child(obj)	
+
+	obj.set_label(number)
+	
+	var n = str(number)[-1]
+	obj.get_node("AnimatedSprite2D").play(n)
+	
+	obj.connect("button_pressed", choice_pressed)
+
+func choice_pressed(index,number):
+	var pour_bottle
+	var textlabel = str(number)
+	
+	if clicked <2:
+		
+		#hapus yg di bawah dulu	
+		delete_bottle_choice(index)
+		
+		#image pour bottle harusnya nanti animasi
+		clicked = clicked + 1
+		if clicked == 1:
+			b_index1 = index
+			machine.set_left(textlabel)
+			
+			pour_bottle = b_pour_left_scene.instantiate()
+			add_child(pour_bottle)
+		
+			pour_bottle.position = Vector2(140,240)
+			
+			
+		if clicked == 2:
+			b_index2 = index
+			machine.set_right(textlabel)
+			
+			pour_bottle = b_pour_right_scene.instantiate()
+			#$bottle_pour.add_child(pour_bottle)			
+			add_child(pour_bottle)
+			pour_bottle.position = Vector2(570,240)
+			
+			
+		#sound harusnya di sini	
+		var n = str(number)[-1]
+		pour_bottle.get_node("AnimatedSprite2D").play(n)
+		await get_tree().create_timer(0.7).timeout
+		pour_bottle.queue_free()
+		
+		
+func delete_bottle_choice(index):
+	for child in $bottle_choice.get_children():
+		if child.index == index:
+			child.queue_free()
+			break
+
+func machine_reset():
+	if clicked>0:
+		for child in $bottle_choice.get_children():
+			child.queue_free()
+
+		generate_choices_bottle()
+		
+		#reset label pada machine
+		if clicked == 1:
+			machine.set_left("")
+		else:	
+			machine.set_left("")
+			machine.set_right("")	
+		
+		#kalau ada result bottle, delete result bottle dan place empty cup
+		#kalau ga ada ya ga usah ngapa2in masih ada empty cup
+		if processed==true:
+			
+			processed = false
+			
+			b_result.queue_free()
+			
+			b_empty = b_empty_scene.instantiate()
+			b_empty.position = Vector2(360,775) 
+			add_child(b_empty)	
+		
+		#reset clicker
+		clicked = 0
+		
+		#reset index
+		b_index1 = null
+		b_index2 = null
+		
+		#reset result
+		result = null
+		
+func machine_process():
+	if clicked == 2:
+		
+		processed = true
+		
+		#hitung result	
+		result = choices[b_index1] + choices[b_index2]
+		
+		#delete emptycup
+		b_empty.queue_free()
+		#b_empty = null
+		
+		#animasi bottle pour result, sebentar lalu dihapus
+		b_pour_result = b_pour_result_scene.instantiate()
+		b_pour_result.position = Vector2(360,757)
+		add_child(b_pour_result)
+		
+		#sound harusnya di sini	
+		var n = str(result)[-1]
+		b_pour_result.get_node("AnimatedSprite2D").play(n)
+		await get_tree().create_timer(0.7).timeout
+		b_pour_result.queue_free()	
+		
+		
+		#taruh bottle result
+		b_result = b_result_scene.instantiate()
+		b_result.position = Vector2(360,775) 
+		add_child(b_result)
+		
+		b_result.get_node("AnimatedSprite2D").play(n)
+		b_result.set_label(result)
--- scripts/machine.gd
+++ scripts/machine.gd
@@ -3,8 +3,14 @@ extends Node2D
 signal reset
 signal go
 
+func set_left(t):
+	$Sprite2D/leftlabel.text = t
+
+func set_right(t):
+	$Sprite2D/rightlabel.text = t
+
 func _on_gobutton_pressed():
 	emit_signal("go")
 
-func _on_cancelbutton_pressed():
+func _on_resetbutton_pressed():
 	emit_signal("reset")
